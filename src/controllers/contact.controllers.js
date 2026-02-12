a8ba608d762727781e072ea7b4c7ffd139dfdc54
Parvesh <arveshchaudhary@gmail.com>
2026-02-12 18:59:04 +0545

    working on express async Handler for manually handel try catch

--- src/controllers/contact.controllers.js
+++ src/controllers/contact.controllers.js
@@ -1,19 +1,19 @@
-
+import asyncHandler from 'express-async-handler'
 
 /*
 -Get all contact
 -  GET /api/contacts
 */
-export const getContacts = (req, res) => {
+export const getContacts = asyncHandler(async (req, res) => {
     res.json({ message: 'Get all contact' });
-};
+});
 
 
 /*
  -Create new contact
  -  POST /api/contacts
  */
-export const creatContact = (req, res) => {
+export const creatContact = asyncHandler(async (req, res) => {
     // console.log("the request body is:", req.body);
 
     const { name, email, phone } = req.body
@@ -22,36 +22,36 @@ export const creatContact = (req, res) => {
         res.status(400);
         throw new Error("All fields are required");
     }
-    
+
     res.json({ message: 'Created a new contact' })
-}
+});
 
 
 /**
 * get contact
 *   GET /api/contacts/:id
 */
-export const getContact = (req, res) => {
+export const getContact = asyncHandler(async (req, res) => {
     res.json({ message: `get contact for ${req.params.id}` })
-}
+});
 
 
 /**
 * Update contact
 *   PUT /api/contacts/:id
 */
-export const updateContact = (req, res) => {
+export const updateContact = asyncHandler(async (req, res) => {
     res.json({ message: `Update a contact for ${req.params.id}` })
-}
+});
 
 
 /**
  * Delete contact
  *   DELETE /api/contacts/:id
  */
-export const deleteContact = (req, res) => {
+export const deleteContact = asyncHandler(async (req, res) => {
     res.json({ message: ' Deteted contact' })
-}
+});
 
 
 
