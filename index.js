b6cad20a399d2626d1a819638af7124dfbe46b94
Parvesh <arveshchaudhary@gmail.com>
2026-02-12 15:23:06 +0545

    Basic setup in index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
 
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT
+
+
+//Test route
+app.get('/',(req,res)=>{
+    res.status(200).json({
+        message:'Hello from backend Team!!'
+    });
+});
+
+
+
+//Listen
+app.listen(PORT,()=>{
+      console.log(`Server is running on http://localhost:${PORT}`);
+})
