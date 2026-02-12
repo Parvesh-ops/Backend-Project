0796fa76d7e6068c1ca1218a36867c3a9671bdab
Parvesh <arveshchaudhary@gmail.com>
2026-02-12 15:33:53 +0545

    Connect Database

---
+++ src/config/database.config.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB Connected at: ${connect.connection.host}:${process.env.PORT}`);
+        
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);
+        process.exit(1);
+    }
+};
