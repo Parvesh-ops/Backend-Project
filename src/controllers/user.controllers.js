a759f26701686a79fcb2ae3a2782a5bf27bce4e3
Parvesh <arveshchaudhary@gmail.com>
2026-02-13 19:21:58 +0545

    userModel, userController and bycrypt to hash password

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,12 +1,45 @@
 import asyncHandler from 'express-async-handler'
+import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
 
 /*Register user
 *  POST /api/user/register
 */
 export const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body  //-> middelware + usermodel
 
-    res.status(201).json({ message: 'register user' })
-});
+    if (!username || !email || !password) {
+        res.status(401);
+        throw new Error("All fields are required");
+    };
+
+    // 🔹 Check if user exists
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
+        res.status(401)
+        throw new Error("Email already exist");
+
+    };
+
+    //hashed password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hassedpassword:", hashedPassword);
+
+    //create newUser
+    const createdUser = await User.create({
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    res.status(201).json({
+        _id: createdUser.id,
+        username: createdUser.username,
+        email: createdUser.email,
+        // password: createdUser.password,
+        message: "User registered successfully",
+    })
+})
 
 
 /*login user
@@ -20,6 +53,6 @@ export const loginUser = asyncHandler(async (req, res) => {
 /*current user
 *  GET /api/user/current
 */
-export const currentUser = (req,res)=>{
-    res.status(201).json({message:'Current user info'})
+export const currentUser = (req, res) => {
+    res.status(201).json({ message: 'Current user info' })
 }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,5 +22,5 @@ const userSchema = new mongoose.Schema(
 
 
 //model
-const User = ("User", userSchema);
+const User = mongoose.model("User", userSchema);
 export default User;
